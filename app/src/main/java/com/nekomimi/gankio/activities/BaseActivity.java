c566e513e766f0b3c47c6d3f9bd92497c31c5776
nekochi <476371546@qq.com>
2016-03-01 16:38:06 +0800

    主界面布局

--- app/src/main/java/com/nekomimi/gankio/activities/BaseActivity.java
+++ app/src/main/java/com/nekomimi/gankio/activities/BaseActivity.java
@@ -1,6 +1,12 @@
 package com.nekomimi.gankio.activities;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by hongchi on 2016-1-27.
@@ -9,6 +15,58 @@ import android.support.v7.app.AppCompatActivity;
 public class BaseActivity extends AppCompatActivity
 {
 
+//    private AppAction mAppAction = null;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState)
+    {
+        super.onCreate(saveInstanceState);
+//        this.mAppAction = new AppActionImpl(this);
+        this.mContext = this;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+//        this.mAppAction = new AppActionImpl(this);
+        this.mContext = this;
+    }
+
+//    public AppAction getAction()
+//    {
+//        return this.mAppAction;
+//    }
+
+    public Context getContext()
+    {
+        return this.mContext;
+    }
+
+    public static class UiHandler extends Handler
+    {
+        private final WeakReference<BaseActivity> mActivity;
+
+        public UiHandler(BaseActivity activity)
+        {
+            mActivity = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg)
+        {
+            BaseActivity activity = mActivity.get();
+            if (activity != null)
+            {
+                activity.handleMessage(msg);
+            }
+        }
+    }
+
+    public void handleMessage(Message msg)
+    {
+
+    }
 
 
 }
--- app/src/main/java/com/nekomimi/gankio/activities/HomeActivity.java
+++ app/src/main/java/com/nekomimi/gankio/activities/HomeActivity.java
@@ -1,8 +1,24 @@
 package com.nekomimi.gankio.activities;
 
+import android.os.Bundle;
+
+import com.nekomimi.gankio.R;
+
 /**
  * Created by hongchi on 2016-3-1.
  * File description :
  */
-public class HomeActivity {
+public class HomeActivity extends BaseActivity
+{
+    private static final String TAG = "HomeActivity";
+
+
+    private UiHandler mUiHandler = new UiHandler(this);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 }
